31523635b6a8ba4c7e3c852679449a8cdf929c1b
YouthfulRicker <76859118+YouthfulRicker@users.noreply.github.com>
2023-01-16 19:48:35 +1300

    hack nerd font install

--- shell-setup.sh
+++ shell-setup.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-sudo apt install zsh
-CHSH $(which zsh)
+# mosh install
+sudo apt install mosh -y
+
+# zsh install
+sudo apt install zsh -y
+chsh -s $(which zsh) $(whoami)
+
+# starship install
 curl -sS https://starship.rs/install.sh | sh
 cp zshrc ~/zshrc
 mv ~/zshrc ~/.zshrc
+
+# 'Hack' nerd font install
+sudo mkdir /usr/share/fonts/truetype/hack
+mkdir hackfont
+cd hackfont
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hack.zip
+unzip Hack.zip
+sudo mv *.ttf /usr/share/fonts/truetype/hack/
+rm -rf ../hackfont
